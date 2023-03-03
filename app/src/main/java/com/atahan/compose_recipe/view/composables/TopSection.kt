acc8f3dbcb2b7047988229d4d18951b461311cc3
cenk atahan özbek <atahanozzbek@gmail.com>
2023-03-03 14:12:44 +0300

    fix ui issues in TopSection.kt

--- app/src/main/java/com/atahan/compose_recipe/view/composables/TopSection.kt
+++ app/src/main/java/com/atahan/compose_recipe/view/composables/TopSection.kt
@@ -1,20 +1,15 @@
 package com.atahan.compose_recipe.view.composables
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.atahan.compose_recipe.R
 
@@ -28,23 +23,20 @@ fun TopSection(modifier: Modifier) {
             modifier = Modifier.fillMaxWidth()
         ) {
 
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_profile_default),
-                    contentDescription = "profile"
-                )
-            }
+            Icon(
+                painter = painterResource(id = R.drawable.ic_profile_default),
+                contentDescription = "profile",
+            )
 
-            Row (
+            Row(
                 horizontalArrangement = Arrangement.Start
-                    ){
+            ) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_location),
                     contentDescription = "location"
                 )
                 Text(text = "Izmir, Turkey")
             }
-
 
         }
 
@@ -57,4 +49,11 @@ fun TopSection(modifier: Modifier) {
 
     }
 
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun Preview() {
+    TopSection(modifier = Modifier.padding(16.dp))
 }
