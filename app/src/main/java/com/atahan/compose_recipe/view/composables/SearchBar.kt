278accc66ec64a32be25f2422822073d11cef67a
cenk atahan özbek <atahanozzbek@gmail.com>
2023-03-08 13:16:09 +0300

    add comments.

--- app/src/main/java/com/atahan/compose_recipe/view/composables/SearchBar.kt
+++ app/src/main/java/com/atahan/compose_recipe/view/composables/SearchBar.kt
@@ -1,28 +1,22 @@
 package com.atahan.compose_recipe.view.composables
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.atahan.compose_recipe.R
 import com.atahan.compose_recipe.ui.theme.AppBlue
-import com.atahan.compose_recipe.ui.theme.AppGrey
-import com.atahan.compose_recipe.ui.theme.AppOrange
-import com.atahan.compose_recipe.ui.theme.Shapes
 
 @Composable
 fun SearchBar(modifier: Modifier = Modifier, searchString: MutableState<String>) {
 
+    //BasicTextField may allow to customize TextField background.
     TextField(
         value = searchString.value,
         onValueChange = {
