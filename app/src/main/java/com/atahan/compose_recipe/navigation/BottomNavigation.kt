81f83b1b4fecebbe80ad845a82a0106473f7dac7
cenk atahan özbek <atahanozzbek@gmail.com>
2023-03-08 14:18:21 +0300

    change name

--- app/src/main/java/com/atahan/compose_recipe/navigation/BottomNavigation.kt
+++ app/src/main/java/com/atahan/compose_recipe/navigation/BottomNavigation.kt
@@ -4,20 +4,20 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.atahan.compose_recipe.model.Screens
+import com.atahan.compose_recipe.model.Screen
 import com.atahan.compose_recipe.view.screen.HomeScreen
 
 @Composable
 fun BottomNavigation(navController: NavHostController) {
 
-    NavHost(navController = navController, startDestination = Screens.Home.route){
-        composable(route = Screens.Home.route){
+    NavHost(navController = navController, startDestination = Screen.Home.route){
+        composable(route = Screen.Home.route){
             HomeScreen()
         }
-        composable(route = Screens.Favorites.route){
+        composable(route = Screen.Favorites.route){
 //            Favorites()
         }
-        composable(route = Screens.MealMenu.route){
+        composable(route = Screen.MealMenu.route){
 //            MealMenu()
         }
     }
