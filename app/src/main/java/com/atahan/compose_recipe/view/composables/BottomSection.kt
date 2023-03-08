7908f07b9caefecb0400684b9f0cf7c3b98a2b08
cenk atahan özbek <atahanozzbek@gmail.com>
2023-03-08 14:18:37 +0300

    add bottom bar.

--- app/src/main/java/com/atahan/compose_recipe/view/composables/BottomSection.kt
+++ app/src/main/java/com/atahan/compose_recipe/view/composables/BottomSection.kt
@@ -1,25 +1,75 @@
 package com.atahan.compose_recipe.view.composables
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material.BottomNavigation
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.atahan.compose_recipe.model.Screens
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.atahan.compose_recipe.model.Screen
+import com.atahan.compose_recipe.ui.theme.AppBlue
 
 @Composable
-fun BottomSection(modifier: Modifier = Modifier) {
-
-    BottomNavigation {
+fun BottomSection(navController: NavHostController, modifier: Modifier = Modifier) {
+    val backStackEntry = navController.currentBackStackEntryAsState().value
+    BottomNavigation(
+        backgroundColor = Color.White,
+        contentColor = AppBlue,
+    ) {
         val screens = arrayListOf(
-            Screens.Home,
-            Screens.Favorites,
-            Screens.MealMenu
+            Screen.Home,
+            Screen.Favorites,
+            Screen.MealMenu
         )
 
-        screens.forEach { bottomNavItem ->
-            Row {
+        screens.forEach { screen ->
+            val selected = screen.route == backStackEntry?.destination?.route
+            BottomNavigationItem(
+                selected = selected,
+                onClick = { /*TODO*/ },
+                icon = {
+                    ChooseIconBy(selected, screen)
+                },
 
-            }
+                )
         }
+    }
+}
+
+@Composable
+private fun ChooseIconBy(isSelected: Boolean, screen: Screen) {
+    when (isSelected) {
+        true ->
+            Column(verticalArrangement = Arrangement.Center) {
+                Icon(
+                    painterResource(id = screen.selectedIcon),
+                    contentDescription = screen.route,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                Text(
+                    text = screen.route,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center
+                )
+            }
+        else ->
+            Column(verticalArrangement = Arrangement.Center) {
+                Icon(
+                    painterResource(id = screen.icon),
+                    contentDescription = screen.route,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
+                Text(
+                    text = screen.route,
+                    fontWeight = FontWeight.Normal,
+                    textAlign = TextAlign.Center
+                )
+            }
     }
 }
--- app/src/main/java/com/atahan/compose_recipe/view/screen/HomeScreen.kt
+++ app/src/main/java/com/atahan/compose_recipe/view/screen/HomeScreen.kt
@@ -7,16 +7,19 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
+import com.atahan.compose_recipe.view.composables.BottomSection
 import com.atahan.compose_recipe.view.composables.SearchBar
 import com.atahan.compose_recipe.view.composables.TopSection
 
 @Composable
 fun HomeScreen() {
+    val navController = rememberNavController()
     val mealSearched = remember { mutableStateOf("") }
 
     Scaffold(
         topBar = { TopSection(modifier = Modifier.padding(16.dp)) },
-        bottomBar = {}
+        bottomBar = { BottomSection(navController = navController) }
     ) {
         SearchBar(
             modifier = Modifier.padding(16.dp),
