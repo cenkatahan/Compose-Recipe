5923ec5f03b7712766f6a5c271fb81daaf78038e
cenk atahan özbek <atahanozzbek@gmail.com>
2023-03-13 14:19:59 +0300

    change meal properties val to var.

--- app/src/main/java/com/atahan/compose_recipe/model/Meal.kt
+++ app/src/main/java/com/atahan/compose_recipe/model/Meal.kt
@@ -1,9 +1,9 @@
 package com.atahan.compose_recipe.model
 
 data class Meal(
-    val name: String,
-    val description: String,
-    val isFavorite: Boolean,
-    val type: MealType,
-    val prepareTime: Int
+    var name: String,
+    var description: String,
+    var isFavorite: Boolean = false,
+    var type: MealType,
+    var prepareTime: Int
 )
