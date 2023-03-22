008cb8c02990b2a0a6b1dc70e43a863dfc982bb0
cenk atahan özbek <atahanozzbek@gmail.com>
2023-03-22 19:10:42 +0300

    update data class to parcelable.

--- app/src/main/java/com/atahan/compose_recipe/model/Meal.kt
+++ app/src/main/java/com/atahan/compose_recipe/model/Meal.kt
@@ -1,10 +1,15 @@
 package com.atahan.compose_recipe.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Meal(
     var name: String,
     var description: String,
     var isFavorite: Boolean = false,
     var type: MealType,
+//    var ingredients: ArrayList<String>,
     var prepareTime: Int,
     var isOnTheMealMenu: Boolean = false
-)
+): Parcelable
