a1086bb8c7e7a867a9985f0e3416095da541236e
cenk atahan özbek <atahanozzbek@gmail.com>
2023-03-22 16:22:53 +0300

    add screen objects.

--- app/src/main/java/com/atahan/compose_recipe/model/Screen.kt
+++ app/src/main/java/com/atahan/compose_recipe/model/Screen.kt
@@ -2,10 +2,13 @@ package com.atahan.compose_recipe.model
 
 import com.atahan.compose_recipe.R
 
-sealed class Screen(val route: String, val icon: Int, val selectedIcon: Int) {
+sealed class Screen(val route: String, val icon: Int? = null, val selectedIcon: Int? = null) {
 
     object Home: Screen("Home", R.drawable.ic_home, R.drawable.ic_home_selected)
     object Favorites: Screen("Favorites", R.drawable.ic_favorite, R.drawable.ic_favorite_selected)
     object MealMenu: Screen("Meal Menu", R.drawable.ic_meal_plan, R.drawable.ic_meal_plan_selected)
+    object Profile: Screen("Meal Menu", R.drawable.ic_meal_plan, R.drawable.ic_meal_plan_selected)
+    object RecipeForm: Screen("Add Recipe")
+    object RecipeDetail: Screen("Recipe Detail")
 
 }
