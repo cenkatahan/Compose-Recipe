56cf2a0637f906a83f19821cc5d4fcc6b3732a64
cenk atahan özbek <atahanozzbek@gmail.com>
2023-03-24 11:17:48 +0300

    add RecipeDetailScreen.kt

--- app/src/main/java/com/atahan/compose_recipe/view/screen/RecipeDetailScreen.kt
+++ app/src/main/java/com/atahan/compose_recipe/view/screen/RecipeDetailScreen.kt
@@ -1,12 +1,23 @@
 package com.atahan.compose_recipe.view.screen
 
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun RecipeDetailScreen() {
-    Scaffold {
-        Text(text = "RecipeDetailScreen")
+
+    //TITLE
+    //BIG IMAGE
+        //FAV BUTTON
+        //MEAL BUTTON
+    //INGREDIENT CHIPS
+
+    //DESCRIPTION
+
+    Column {
+//        Text(text = meal.name, fontSize = 40.sp)
     }
+
+    Text(text = "RecipeDetailScreen")
 }
