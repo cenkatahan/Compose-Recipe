65ec82e2b3127087e9a0d1e1f24f0323971ba61c
cenk atahan özbek <atahanozzbek@gmail.com>
2023-03-24 11:16:52 +0300

    add data class id

--- app/src/main/java/com/atahan/compose_recipe/model/Meal.kt
+++ app/src/main/java/com/atahan/compose_recipe/model/Meal.kt
@@ -2,9 +2,11 @@ package com.atahan.compose_recipe.model
 
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
+import java.util.UUID
 
 @Parcelize
 data class Meal(
+    val id: String = UUID.randomUUID().toString(),
     var name: String,
     var description: String,
     var isFavorite: Boolean = false,
