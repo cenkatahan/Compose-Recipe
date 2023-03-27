fd63284e0f0efacea973ae74558f765bb42639bc
cenk atahan özbek <atahanozzbek@gmail.com>
2023-03-27 15:46:23 +0300

    add fields to add screen.

--- app/src/main/java/com/atahan/compose_recipe/view/screen/RecipeFormScreen.kt
+++ app/src/main/java/com/atahan/compose_recipe/view/screen/RecipeFormScreen.kt
@@ -1,12 +1,45 @@
 package com.atahan.compose_recipe.view.screen
 
-import androidx.compose.material.Scaffold
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun RecipeFormScreen() {
-    Scaffold {
-        Text(text = "RecipeFormScreen")
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth()
+    ) {
+
+        Text(
+            text = "Add a Recipe",
+            style = MaterialTheme.typography.h2,
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "Recipe Name") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "Recipe Description") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+
     }
+
+
 }
