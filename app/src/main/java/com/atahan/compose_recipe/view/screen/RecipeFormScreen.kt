607b0a52b741726aa610b5238b32725af6aac71c
cenk atahan özbek <atahanozzbek@gmail.com>
2023-04-04 15:01:04 +0300

    add top bar to form screen.

--- app/src/main/java/com/atahan/compose_recipe/view/screen/RecipeFormScreen.kt
+++ app/src/main/java/com/atahan/compose_recipe/view/screen/RecipeFormScreen.kt
@@ -2,47 +2,61 @@ package com.atahan.compose_recipe.view.screen
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.atahan.compose_recipe.navigation.Graph
+import com.atahan.compose_recipe.view.composables.FormTopBar
 
 @Composable
 fun RecipeFormScreen(
     navController: NavHostController
 ) {
 
-    Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxWidth()
+    Scaffold(
+        topBar = {
+            FormTopBar(
+                onClickHome = { navController.navigate(Graph.APP_GRAPH) },
+                onClickConfirmAdd = {
+                    //TODO implement code here.
+                    navController.navigate(Graph.APP_GRAPH)
+                }
+            )
+        }
     ) {
 
-        Text(
-            text = "Add a Recipe",
-            style = MaterialTheme.typography.h2,
-        )
-        Spacer(modifier = Modifier.height(8.dp))
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        ) {
 
-        TextField(
-            value = "",
-            onValueChange = {},
-            label = { Text(text = "Recipe Name") },
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "Add a Recipe",
+                style = MaterialTheme.typography.h2,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
 
-        TextField(
-            value = "",
-            onValueChange = {},
-            label = { Text(text = "Recipe Description") },
-            modifier = Modifier.fillMaxWidth()
-        )
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text(text = "Recipe Name") },
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.height(8.dp))
 
+            TextField(
+                value = "",
+                onValueChange = {},
+                label = { Text(text = "Recipe Description") },
+                modifier = Modifier.fillMaxWidth()
+            )
+
+        }
 
     }
-
-
 }
