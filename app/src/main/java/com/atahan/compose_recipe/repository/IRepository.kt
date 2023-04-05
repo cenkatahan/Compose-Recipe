60dfe3fabe5c6b1702bd5b2ec965a19e16aedd39
cenk atahan özbek <atahanozzbek@gmail.com>
2023-04-05 16:20:42 +0300

    Revert "Revert "add repository""
    This reverts commit a7d6a0cf9259d82d82268c5c743135b9d7e1dd87.

---
+++ app/src/main/java/com/atahan/compose_recipe/repository/IRepository.kt
@@ -0,0 +1,11 @@
+package com.atahan.compose_recipe.repository
+
+import com.atahan.compose_recipe.model.Recipe
+
+interface IRepository {
+    fun getRecipes(): List<RecipeRepo>
+    fun getRecipeBy(id: Int): Recipe
+    fun removeBy(id: Int)
+    fun add(recipe: Recipe)
+    fun update(recipe: Recipe)
+}
