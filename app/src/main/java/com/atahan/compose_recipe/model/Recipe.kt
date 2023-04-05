5fd9ef8063e9b9a4d121004eee1dc80123bb752e
cenk atahan özbek <atahanozzbek@gmail.com>
2023-04-05 12:06:41 +0300

    add room classes.

--- app/src/main/java/com/atahan/compose_recipe/model/Recipe.kt
+++ app/src/main/java/com/atahan/compose_recipe/model/Recipe.kt
@@ -1,17 +1,37 @@
 package com.atahan.compose_recipe.model
 
 import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.atahan.compose_recipe.common.Common.COL_DESC
+import com.atahan.compose_recipe.common.Common.COL_INGREDIENTS
+import com.atahan.compose_recipe.common.Common.COL_IS_FAV
+import com.atahan.compose_recipe.common.Common.COL_MEAL_TYPE
+import com.atahan.compose_recipe.common.Common.COL_NAME
+import com.atahan.compose_recipe.common.Common.COL_ON_MENU
+import com.atahan.compose_recipe.common.Common.COL_PREP_TIME
+import com.atahan.compose_recipe.common.Common.TABLE_NAME
 import com.atahan.compose_recipe.enums.MealType
 import kotlinx.parcelize.Parcelize
 
+@Entity(tableName = TABLE_NAME)
 @Parcelize
 data class Recipe(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
+    @ColumnInfo(name = COL_NAME)
     var name: String,
+    @ColumnInfo(name = COL_DESC)
     var description: String,
+    @ColumnInfo(name = COL_IS_FAV)
     var isFavorite: Boolean = false,
+    @ColumnInfo(name = COL_MEAL_TYPE)
     var type: MealType,
+    @ColumnInfo(name = COL_INGREDIENTS)
     var ingredients: List<String>,
+    @ColumnInfo(name = COL_PREP_TIME)
     var prepareTime: Int,
+    @ColumnInfo(name = COL_ON_MENU)
     var isOnTheMealMenu: Boolean = false
 ): Parcelable
