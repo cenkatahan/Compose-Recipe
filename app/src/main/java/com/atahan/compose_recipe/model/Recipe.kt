ec4e1e8172e608eee9ff42e0b39fdb779d7791dd
cenk atahan özbek <atahanozzbek@gmail.com>
2023-04-05 14:50:10 +0300

    Revert "update model class."
    This reverts commit 77ccc4406f961b2f8111f5d06c3cd9dc925ddd86.

--- app/src/main/java/com/atahan/compose_recipe/model/Recipe.kt
+++ app/src/main/java/com/atahan/compose_recipe/model/Recipe.kt
@@ -19,19 +19,19 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class Recipe(
     @PrimaryKey(autoGenerate = true)
-    val id: Int = 0,
+    val id: Int,
     @ColumnInfo(name = COL_NAME)
-    var name: String = "",
+    var name: String,
     @ColumnInfo(name = COL_DESC)
-    var description: String = "",
+    var description: String,
     @ColumnInfo(name = COL_IS_FAV)
     var isFavorite: Boolean = false,
     @ColumnInfo(name = COL_MEAL_TYPE)
-    var type: MealType = MealType.DESSERT,
+    var type: MealType,
     @ColumnInfo(name = COL_INGREDIENTS)
-    var ingredients: List<String> = listOf(),
+    var ingredients: List<String>,
     @ColumnInfo(name = COL_PREP_TIME)
-    var prepareTime: Int = 0,
+    var prepareTime: Int,
     @ColumnInfo(name = COL_ON_MENU)
     var isOnTheMealMenu: Boolean = false
 ): Parcelable
