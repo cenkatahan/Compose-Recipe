2fb4c1d97ee98ee46235a498bd2ea24e3b177eaa
cenk atahan özbek <atahanozzbek@gmail.com>
2023-04-26 15:28:28 +0300

    update Success from Resource.kt

--- app/src/main/java/com/atahan/compose_recipe/util/Resource.kt
+++ app/src/main/java/com/atahan/compose_recipe/util/Resource.kt
@@ -1,8 +1,8 @@
 package com.atahan.compose_recipe.util
 
 sealed class Resource<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T) : Resource<T>(data)
-    class Valid<T> : Resource<T>()
+
+    class Success<T>(data: T? = null) : Resource<T>(data)
     class Error<T>(message: String, data: T? = null) : Resource<T>(data, message)
     class Loading<T> : Resource<T>()
 }
