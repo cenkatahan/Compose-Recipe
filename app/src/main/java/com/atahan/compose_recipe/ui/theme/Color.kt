086791a4ab82737a60d1ce6fcf47e47973d81893
cenk atahan özbek <atahanozzbek@gmail.com>
2023-05-08 15:47:38 +0300

    update gray color.

--- app/src/main/java/com/atahan/compose_recipe/ui/theme/Color.kt
+++ app/src/main/java/com/atahan/compose_recipe/ui/theme/Color.kt
@@ -2,11 +2,9 @@ package com.atahan.compose_recipe.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
 val AppBlue = Color(0XFF0F172E)
-val AppGrey = Color(0xFFD8D8D8)
-val AppOrange = Color(0xFFFF5722)
+val AppGrey = Color(0xA6D8D8D8)
+//val AppOrange = Color(0xFFFF5722)
