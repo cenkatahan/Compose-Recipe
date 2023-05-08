989057b121fe2aa4c5bf3798d07f6381421f0e10
cenk atahan özbek <atahanozzbek@gmail.com>
2023-05-08 14:35:31 +0300

    add property to enum classes

--- app/src/main/java/com/atahan/compose_recipe/enums/Category.kt
+++ app/src/main/java/com/atahan/compose_recipe/enums/Category.kt
@@ -1,12 +1,12 @@
 package com.atahan.compose_recipe.enums
 
-enum class Category {
-    ALL,
-    SOUP,
-    MEAT,
-    VEGETABLE,
-    DESSERT,
-    PASTRY,
-    FRUIT,
-    FAST_FOOD
+enum class Category(val title: String) {
+    ALL("ALL"),
+    SOUP("SOUP"),
+    MEAT("MEAT"),
+    VEGETABLE("VEGETABLE"),
+    DESSERT("DESSERT"),
+    PASTRY("PASTRY"),
+    FRUIT("FRUIT"),
+    FAST_FOOD("FAST FOOD")
 }
--- app/src/main/java/com/atahan/compose_recipe/enums/MealType.kt
+++ app/src/main/java/com/atahan/compose_recipe/enums/MealType.kt
@@ -1,11 +1,11 @@
 package com.atahan.compose_recipe.enums
 
-enum class MealType(val icon: Int) {
-    SOUP(0),
-    MEAT(0),
-    VEGETABLE(0),
-    DESSERT(0),
-    PASTRY(0),
-    FRUIT(0),
-    FAST_FOOD(0)
+enum class MealType(val icon: Int, val title: String) {
+    SOUP(0, "SOUP"),
+    MEAT(0, "MEAT"),
+    VEGETABLE(0, "VEGETABLE"),
+    DESSERT(0, "DESSERT"),
+    PASTRY(0, "PASTRY"),
+    FRUIT(0, "FRUIT"),
+    FAST_FOOD(0, "FAST FOOD")
 }
