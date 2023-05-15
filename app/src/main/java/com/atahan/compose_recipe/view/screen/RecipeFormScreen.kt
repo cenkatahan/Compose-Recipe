8d15a2b98b56c4d5dffb3cbd285a9dcc6f03966c
cenk atahan özbek <atahanozzbek@gmail.com>
2023-05-15 16:29:54 +0300

    update form screen.

--- app/src/main/java/com/atahan/compose_recipe/view/screen/RecipeFormScreen.kt
+++ app/src/main/java/com/atahan/compose_recipe/view/screen/RecipeFormScreen.kt
@@ -2,19 +2,20 @@ package com.atahan.compose_recipe.view.screen
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.atahan.compose_recipe.R
 import com.atahan.compose_recipe.navigation.Graph
 import com.atahan.compose_recipe.navigation.Screen
 import com.atahan.compose_recipe.view.composables.FormTopBar
 
-@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter", "MutableCollectionMutableState")
 @Composable
 fun RecipeFormScreen(
     navController: NavHostController
@@ -54,13 +55,34 @@ fun RecipeFormScreen(
             )
             Spacer(modifier = Modifier.height(8.dp))
 
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = { Text(text = "Recipe Description") },
-                modifier = Modifier.fillMaxWidth()
-            )
+            Column {
 
+                var descriptions by remember {
+                    mutableStateOf(1)
+                }
+
+                (0 until descriptions).forEach {
+                    Spacer(modifier = Modifier.height(4.dp))
+                    TextField(
+                        value = "",
+                        onValueChange = {},
+                        label = { Text(text = "Recipe Description") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+
+                Button(
+                    onClick = {
+                        descriptions++
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_add_circle),
+                        contentDescription = "add new description",
+                    )
+                }
+            }
         }
 
     }
